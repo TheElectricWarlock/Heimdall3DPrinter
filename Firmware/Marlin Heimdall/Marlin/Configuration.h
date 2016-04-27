5feb042161800d40e26987b3adfc2f9e2c36fa31
Angel Mercedes <angel.mercedes20@gmail.com>
2016-04-27 19:56:27 -0400

    Changes in the Readme file as well as the Firmware

--- Firmware/Marlin Heimdall/Marlin/Configuration.h
+++ Firmware/Marlin Heimdall/Marlin/Configuration.h
@@ -334,11 +334,11 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 187
+#define X_MAX_POS 180
 #define X_MIN_POS 0
-#define Y_MAX_POS 225
+#define Y_MAX_POS 220
 #define Y_MIN_POS 0
-#define Z_MAX_POS 170
+#define Z_MAX_POS 230
 #define Z_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
@@ -479,7 +479,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.00, 80.00 ,2267.72 ,100}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.00, 80.00 ,2267.72 ,100}  
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 25}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
--- README.md
+++ README.md
@@ -3,4 +3,3 @@ The Heimdall is a CoreXY 3D Printer capable of fast and reliable printing. It us
 
 For more information regarding the printer visit the project's wiki.
 
-For updates, pictures, and other information visit my blog: http://angeldmercedes.weebly.com/blog/category/heimdall
